8f63e29b4a89ee0b6dd384fd47ba7959cd66df55
MadnessASAP <michael@a5ap.net>
2012-06-19 03:07:07 -0400

    Modified putchar to support newlines, needs testing. May be a bad idea overall anyways

--- libc/stdio/putchar.c
+++ libc/stdio/putchar.c
@@ -1,5 +1,6 @@
 #include <dcpu.h>
 #include <stdio.h>
+#include <string.h>
 
 
 // This function takes a char and places it onto the screen into the next position on the display
@@ -7,7 +8,14 @@
 // This function ultimately handles all writes to the screen and should therefore be as fast as possible
 int putchar( int ch ) {
 	static unsigned pos = 0;
-	if (pos >= SCREEN_WIDTH * SCREEN_HEIGHT) pos = 0;
+	if ( ch == '\n' ) {
+		pos = ((pos/SCREEN_WIDTH) + 1) * SCREEN_WIDTH;
+		return ch;
+	}		
+	if (pos >= SCREEN_WIDTH * SCREEN_HEIGHT) {
+		memcpy( _display, _display+SCREEN_WIDTH, (SCREEN_HEIGHT - 1) * SCREEN_WIDTH );	// Copy screen up 1 line
+		memset( _display + (SCREEN_HEIGHT - 1) * SCREEN_WIDTH, ' ', SCREEN_WIDTH );		// Blank last line
+	}
 
 	_display[pos++] = LEM_GLYPH( ch, 0, 0xF, 0x0 );
     return ch;
